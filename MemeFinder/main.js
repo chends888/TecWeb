5bb90379fbd89abce0c5e6b63dc28f408c912e22
chends888 <lchends@gmail.com>
2018-05-31 13:41:30 -0300

    Update projeto final

--- MemeFinder/main.js
+++ MemeFinder/main.js
@@ -8,13 +8,32 @@ var connection = mysql.createConnection({
     database: 'memefinder'
 });
 
-app.get('/', function(req, res){
-    connection.query('SELECT * FROM Favorite WHERE user_id = 2', function(error, results, fields) {
-        if(error) throw error;
+app.get('/', function (req, res) {
+    connection.query('SELECT * FROM Favorite WHERE user_id = 2', function (error, results, fields) {
+        if (error) throw error;
         res.json(results)
     });
 });
 
-app.listen(3200, function() {
+var api = "http://api.giphy.com/v1/gifs/"
+var gifId = "xT4uQulxzV39haRFjG?"
+var apiKey = "&api_key=YC8Q6IsUVRDc898LRu8UEUhVodqYv6CT"
+
+function setup() {
+    noCanvas();
+    var url = api + gifId + apiKey;
+    loadJSON(url, gotData);
+}
+
+
+function gotData(data) {
+    println(data);
+}
+
+// https://malcoded.com/posts/angular-backend-express
+// app.route('/trending').get((req, res) => {});
+
+
+app.listen(3300, function () {
     console.log('Server running on port 3200')
 });
